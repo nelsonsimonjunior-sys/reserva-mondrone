abfcae82d179851ca5a5103c9561566364ef9e8c
nelsonsimonjunior-sys <nelson.simon.junior@escola.pr.gov.br>
2026-09-10 01:22:34 -0300

    Update app.py

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ from streamlit_gsheets import GSheetsConnection
 st.set_page_config(
     page_title="Reserva de Equipamentos - Colégio Mondrone", page_icon="LogoMondrone.jpg"
 )
-
-st.title("LogoMondrone.jpg Reserva de Equipamentos - Colégio Mondrone")
+page_icon="LogoMondrone.jpg
+st.title("Reserva de Equipamentos - Colégio Mondrone")
 st.write("Selecione os dados abaixo para agendar um equipamento.")
 
 # Conexão com a planilha do Google Sheets
