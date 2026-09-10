96002682deafcb6ac24c94b23073d2e6666b6eec
nelsonsimonjunior-sys <nelson.simon.junior@escola.pr.gov.br>
2026-09-10 01:20:52 -0300

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ st.set_page_config(
     page_title="Reserva de Equipamentos - Colégio Mondrone", page_icon="LogoMondrone.jpg"
 )
 
-st.title(":LogoMondrone.jpg: Reserva de Equipamentos - Colégio Mondrone")
+st.title("LogoMondrone.jpg Reserva de Equipamentos - Colégio Mondrone")
 st.write("Selecione os dados abaixo para agendar um equipamento.")
 
 # Conexão com a planilha do Google Sheets
